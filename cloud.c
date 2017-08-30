9a40f43aefb146bd7553f8a22f159555d8a1661d
jiadong <328961630@qq.com>
2017-08-30 16:02:29 +0800

    使用多路复用实现视频共享

--- cloud.c
+++ cloud.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
 #include "tcp_servers.h"
 
+int readhead(int fd, void *buf)
+{
+	unsigned char *tmp = (unsigned char *)buf;
+	int nread, total = 0;
+	while(1)
+	{
+		nread = read(fd, tmp+total, 1);
+		if(nread == 0)
+			return 0;
+		total += nread;
+		if(strstr(tmp, "\r\n\r\n"))
+			break;
+	}
+
+	return total;
+}
+
+int reader(int fd, void *buf, int size)
+{
+	unsigned char *tmp = (unsigned char *)buf;
+	int nread, total = 0;
+	while(1)
+	{
+		nread = read(fd, tmp+total, size-total);
+		if(nread == 0)
+			return 0;
+		total += nread;
+		if(total == size)
+			return total;
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	int fd = tcp_servers(atoi(argv[1]));
@@ -17,6 +49,7 @@ int main(int argc, char *argv[])
 	int fd_max = fd;
 	fd_set readfds;
 	list_readfd *tmp;
+	unsigned char *head_msg = malloc(256);
 	unsigned char *msg = malloc(BUFSIZE);
 	while(1)
 	{
@@ -26,61 +59,37 @@ int main(int argc, char *argv[])
 		{
 			FD_SET(tmp->fd, &readfds);
 			fd_max = tmp->fd>fd_max? tmp->fd: fd_max;
-			printf("Welcome [%s:%d] %d\n", 
+			printf(" [%s:%d] %d\n", 
 				inet_ntoa(tmp->clinet.sin_addr),
 				ntohs(tmp->clinet.sin_port), tmp->type);
 		}
-
+#ifdef DEBUG
+		printf("%d\n", __LINE__);
+#endif
 		select(fd_max+1, &readfds, NULL, NULL, 0);
 		if(FD_ISSET(fd, &readfds))
 		{
 			bzero(&client, len);
 			int fdtmp = accept(fd, (struct sockaddr *)&client, &len);
-			printf(" [%s:%d]\n", 
+			printf("Welcome [%s:%d]\n", 
 				inet_ntoa(client.sin_addr),
 				ntohs(client.sin_port));
 			list_readfd *new = new_readfd(fdtmp, &client);
 			list_add_tail(&new->list, &head->list);
 		}
+#ifdef DEBUG
+		printf("%d\n", __LINE__);
+#endif
 
 		list_for_each_entry(tmp, &head->list, list)
 		{
 			if(FD_ISSET(tmp->fd, &readfds))
 			{
-				bzero(msg, BUFSIZE);
-				int nread, total = 0;
-				unsigned char *p = msg;
-				int ret;
-				while((ret=read(tmp->fd, msg, 1)) == 0);
-				printf("msg: %s\n", msg);
-				if(*msg == '0')
-				{
+				bzero(head_msg, 256);
+				int ret = readhead(tmp->fd, head_msg);
 #ifdef DEBUG
-				printf("[%d]\n", __LINE__);
+		printf("%d\n", __LINE__);
 #endif
-					while(1)
-					{
-						nread = read(tmp->fd, p+total, 8-total);
-						total += nread;
-						if(total == 8)
-							break;
-					}
-				printf("msg: %s\n", msg);
-				}
-				else if(*msg == '1')
-				{
-#ifdef DEBUG
-				printf("[%d]\n", __LINE__);
-#endif
-					while(1)
-					{
-						nread = read(tmp->fd, p+total, BUFSIZE-total);
-						total += nread;
-						if(total == BUFSIZE)
-							break;
-						
-					}
-				}
 				if(ret == 0)
 				{
 					/*删除断开的节点*/
@@ -93,7 +102,7 @@ int main(int argc, char *argv[])
 					tmp = pos;
 					continue;
 				}
-				if(strstr(msg, "send\r\n\r\n"))
+				if(strstr(head_msg, "send\r\n\r\n"))
 				{
 					tmp->type = SEND;
 					continue;
@@ -101,11 +110,16 @@ int main(int argc, char *argv[])
 
 				if(tmp->type == SEND)
 				{
+					bzero(msg, BUFSIZE);
+					reader(tmp->fd, msg, atoi(head_msg));
+#ifdef DEBUG
+		printf("%d\n", __LINE__);
+#endif
 					list_readfd *ctrl;
 					list_for_each_entry(ctrl, &head->list, list)
 					{
 						if(ctrl->type == RECV)
-							write(ctrl->fd, msg, BUFSIZE);
+							write(ctrl->fd, msg, atoi(head_msg));
 					}
 				}
 			}
